d0513b89c23e3ee17c560516889e8c343b1564ea
kevinhou168 <kevinhou168@gmail.com>
2019-07-02 14:00:24 -0500

    Add state modifying function

--- game1.py
+++ game1.py
@@ -1,25 +1,44 @@
 import multi_pill_sort
 import pyhop
 
+
 def get_goal():
-  goal = pyhop.Goal('goal')
-  goal.days = {}      # Outer array: Day of week, Inner array: Time of day, Value: # of pills present
-  goal.days['Green'] = [[1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0]]
-  goal.days['Blue'] = [[0,2,0,0], [0,0,0,0], [2,0,0,0], [0,0,0,0], [2,0,0,0], [0,0,0,0], [0,0,0,0]]
-  #goal.light = 'on'
-  return goal
+    goal = pyhop.Goal('goal')
+    goal.days = {}      # Outer array: Day of week, Inner array: Time of day, Value: # of pills present
+    goal.days['Green'] = [[1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0]]
+    goal.days['Blue'] = [[0,2,0,0], [0,0,0,0], [2,0,0,0], [0,0,0,0], [2,0,0,0], [0,0,0,0], [0,0,0,0]]
+    #goal.light = 'on'
+    return goal
+
 
 # ph = planners.pyhop.pyhop.Pyhop('hoppity')
 # methods.load_methods(ph)
 # operators.load_operators(ph)
 
-def get_start_state():
-  state = pyhop.State('init')
-  state.days = {}
-  state.days['Green'] = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-  state.days['Blue'] = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-  state.pills = list(state.days.keys())
-  return state
 
-print(get_goal())
-pyhop.pyhop(get_start_state(), [('sort_meds', get_goal())], verbose = 2)
+def get_start_state():
+    state = pyhop.State('init')
+    state.days = {}
+    state.days['Green'] = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    state.days['Blue'] = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    state.pills = list(state.days.keys())
+    return state
+
+
+def modify_state(state, pill, day, time, action):
+    if action == 'add_pill':
+        return multi_pill_sort.add_pill(state, pill, day, time)
+    elif action == 'remove_pill':
+        return multi_pill_sort.remove_pill(state, pill, day, time)
+    else
+        raise Exception('')
+
+
+
+def main():
+    start_state = get_start_state()
+    for()
+    pyhop.pyhop(get_start_state(), [('sort_meds', get_goal())], verbose=2)
+
+if __name__ == '__main__':
+    main()
